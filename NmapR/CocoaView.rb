c09c76b9316156c69b7fe7612bebbd0834583e32
larmeh <larmeh@gmail.com>
2013-02-06 19:12:13 +0100

    Added skeleton for CocoaView.

--- NmapR/CocoaView.rb
+++ NmapR/CocoaView.rb
@@ -5,3 +5,9 @@
 #  Created by Lars Mehrtens on 06.02.13.
 #  Copyright 2013 AWE13. All rights reserved.
 #
+
+require 'osx/cocoa'
+OSX.require_framework 'CoreData'
+
+class CocoaView < OSX::NSView
+end
