df8d7693a40a16c89c06ad7f5d9fab045213c18a
bentzl <bentz@tzi.de>
2013-02-06 19:49:23 +0100

    added variables for nmap settings to nmapModel

--- NmapR/NmapModel.rb
+++ NmapR/NmapModel.rb
@@ -1,20 +1,25 @@
 require 'rubygems'
 require 'nmap/program'
 
-attr_accessor :targets, :ports
+class NmapModel
 
-def scan()
-	Nmap::Program.scan do |nmap|
-        ## these are disabled since they require root
-        nmap.syn_scan = false
-        nmap.os_fingerprint = false
-        ##
-        
-        nmap.service_scan = true
-        nmap.xml = 'scan.xml'
-        nmap.verbose = false
-        nmap.ports = ports
-        nmap.targets = targets
-	end
+    attr_accessor :targets, :ports, :service_scan, :verbose, :skip_discovery
     
+    def scan()
+        Nmap::Program.scan do |nmap|
+            ## these are disabled since they require root
+            nmap.syn_scan = false
+            nmap.os_fingerprint = false
+            ##
+            
+            nmap.service_scan = true
+            nmap.verbose = false
+            nmap.xml = true
+            nmap.ports = ports
+            nmap.targets = targets
+            nmap.skip_discovery = skip_discovery
+        end
+        
+    end
+
 end
